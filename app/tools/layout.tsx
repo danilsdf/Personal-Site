db0a5d28c0ffae959a3bba8339b7df6f226ab360
Danil Kravchenko <danilk@blazesoft.ca>
2026-02-09 11:48:33 -0500

    done

--- app/tools/layout.tsx
+++ app/tools/layout.tsx
@@ -1,13 +1,5 @@
-// app/layout.tsx
 import MainHeader from "@/components/headers/MainHeader";
-import "./../globals.css";
-import type { Metadata } from "next";
 import MainFooter from "@/components/footer/MainFooter";
-
-export const metadata: Metadata = {
-  title: "Danil Kravchenko | Athlete & Coder",
-  description: "Hybrid athlete & software engineer.",
-};
 
 export default function MainLayout({
   children,
